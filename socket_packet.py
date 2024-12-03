b8d9e47859b653f46c1ff8ad9f1c9460636eb446
YFshadaow <renzd2001@gmail.com>
2024-12-03 19:51:18 +0800

    Add socket_packet.py

---
+++ socket_packet.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class PacketType(Enum):
+    HEARTBEAT_TO_BOT = 0
+    SERVER_CHAT_TO_BOT = 1
+    GROUP_CHAT_TO_SERVER = 2
+    PLAYER_LOOKUP_REQUEST_TO_BOT = 3
+    PLAYER_LOOKUP_RESULT_TO_SERVER = 4
+    PLAYER_NOT_FOUND_TO_SERVER = 5
+    LIST_REQUEST_TO_SERVER = 6
+    COMMAND_TO_SERVER = 7
+    SERVER_INFORMATION_TO_BOT = 8
+
+class SocketPacket:
+
+    def __init__(self, packet_type, content: str):
+        self.packet_type = packet_type
+        self.content = content
+
+    @classmethod
+    def from_dict(cls, d: dict):
+        return cls(PacketType[d["packetType"]],  d["content"])
+
+    def to_dict(self):
+        result = dict()
+        result["packetType"] = self.packet_type.name
+        result["content"] = self.content
+        return result
